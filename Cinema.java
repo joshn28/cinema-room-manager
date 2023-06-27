099ee48399b4ef0dd2f99115c12c57a0ccec3729
Joshua Nobleza <joshnobleza@bell.net>
2023-06-27 16:52:37 -0400

    feat: Calculate the profit from all the sold tickets

--- Cinema.java
+++ Cinema.java
@@ -1,5 +1,7 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
@@ -13,5 +15,27 @@ public class Cinema {
             5 S S S S S S S S
             6 S S S S S S S S
             7 S S S S S S S S""");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+
+        System.out.println("Enter the number of seats in each row:");
+        int seatsInRow = scanner.nextInt();
+
+        System.out.println("Total Income:");
+        int totalSeats = rows * seatsInRow;
+
+        int profit;
+        if (totalSeats <= 60) {
+            profit = totalSeats * 10;
+        } else {
+            int frontSeatsPrice = rows / 2 * seatsInRow * 10;
+            int backSeatsPrice = (rows - rows / 2) * seatsInRow * 8;
+            profit = frontSeatsPrice + backSeatsPrice;
+        }
+
+        System.out.printf("$%d", profit);
     }
 }
