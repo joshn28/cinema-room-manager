63eb0958b32431fe7e66ea80d8deda4fe8f78675
Joshua Nobleza <joshnobleza@bell.net>
2023-06-27 16:33:41 -0400

    feat: Visualize the seating arrangement

---
+++ Cinema.java
@@ -0,0 +1,17 @@
+package cinema;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+        System.out.print("""
+            Cinema:
+              1 2 3 4 5 6 7 8
+            1 S S S S S S S S
+            2 S S S S S S S S
+            3 S S S S S S S S
+            4 S S S S S S S S
+            5 S S S S S S S S
+            6 S S S S S S S S
+            7 S S S S S S S S""");
+    }
+}
